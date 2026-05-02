a80111e7f03623ad5b09210dac4062981fb42a9a
Ranya227 <raniaabker27@gmail.com>
2026-05-02 16:28:00 +0300

    fix duplicate

--- clothes_store/wsgi.py
+++ clothes_store/wsgi.py
@@ -1,16 +1,27 @@
 """
 WSGI config for clothes_store project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothes_store.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    
+    username = 'ranya'
+    password = '1234'
+    email = 'ranya@example.com'
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f" Success: Superuser '{username}' has been created.")
+    else:
+        print(f" Info: Superuser '{username}' already exists.")
+        
+except Exception as e:
+    print(f"Error creating superuser: {e}")
